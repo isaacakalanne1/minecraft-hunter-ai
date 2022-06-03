34071f7a5d18a1d4981fa9387cc172a36e722943
Isaac Akalanne <iakalann@jaguarlandrover.com>
2022-06-03 23:16:43 +0100

    Created MainRunner file, and updated comment with potential explanation for why listener needs to be kept there

---
+++ MainRunner.py
@@ -0,0 +1,9 @@
+from javascript import require, On
+import Hunter
+
+hunter = Hunter.Hunter('localHost', 51238, 'HelloThere')
+
+# It seems like if this listener isn't placed here, the Python file assumes it only needs to run briefly, and stops itself running
+@On(hunter.bot, 'eventNeverUsed')
+def h(*args):
+  pass
