3b6bbad5ab83bb18925ed7b59352f3bd5d07b51b
Isaac Akalanne <iakalann@jaguarlandrover.com>
2022-06-06 10:58:04 +0100

    Added MCPi code which allows user to connect to LAN server

--- MainRunner.py
+++ MainRunner.py
@@ -1,38 +1,42 @@
 from javascript import require, On
+from mcpi_e.minecraft import Minecraft
 
 flyingSquid = require('/Users/iakalann/node_modules/flying-squid')
+mc = Minecraft.create('localHost', 25652, 'RoyalCentaur')
+pos = mc.player.getPos()
 
-test = "test"
+print("pos: x:{},y:{},z:{}".format(pos.x,pos.y,pos.z))
+# test = "test"
 
-flyingSquid.createMCServer({
-  'motd': 'A Minecraft Server \nRunning flying-squid',
-  'port': 25565,
-  'max-players': 10,
-  'online-mode': False,
-  'logging': True,
-  'gameMode': 0,
-  'difficulty': 1,
-  'worldFolder': 'world',
-  'generation': {
-    'name': 'empty',
-    'options': {
-      'worldHeight': 80
-    }
-  },
-  'kickTimeout': 10000,
-  'plugins': {
+# flyingSquid.createMCServer({
+#   'motd': 'A Minecraft Server \nRunning flying-squid',
+#   'port': 25565,
+#   'max-players': 10,
+#   'online-mode': False,
+#   'logging': True,
+#   'gameMode': 0,
+#   'difficulty': 1,
+#   'worldFolder': 'world',
+#   'generation': {
+#     'name': 'empty',
+#     'options': {
+#       'worldHeight': 80
+#     }
+#   },
+#   'kickTimeout': 10000,
+#   'plugins': {
 
-  },
-  'modpe': False,
-  'view-distance': 10,
-  'player-list-text': {
-    'header': 'Flying squid',
-    'footer': 'Test server'
-  },
-  'everybody-op': False,
-  'max-entities': 100,
-  'version': '1.18'
-})
+#   },
+#   'modpe': False,
+#   'view-distance': 10,
+#   'player-list-text': {
+#     'header': 'Flying squid',
+#     'footer': 'Test server'
+#   },
+#   'everybody-op': False,
+#   'max-entities': 100,
+#   'version': '1.18'
+# })
 
-while True:
-    pass
+# while True:
+#     pass
