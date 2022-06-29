079543ead3da87bb446dd6de6014333e7c0bc522
Isaac Akalanne <iakalann@jaguarlandrover.com>
2022-06-29 14:17:10 +0100

    Deleted unused file

--- MainRunner.py
+++
@@ -1,8 +0,0 @@
-from javascript import require, On
-from mcpi_e.minecraft import Minecraft
-
-flyingSquid = require('/Users/iakalann/node_modules/flying-squid')
-mc = Minecraft.create('localHost', 25652, 'RoyalCentaur')
-pos = mc.player.getPos()
-
-print("pos: x:{},y:{},z:{}".format(pos.x,pos.y,pos.z))
